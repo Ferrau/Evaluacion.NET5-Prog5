03dfdcff03fbc6d0a655c90b8c0af451dbe3f9f2
Ester Ferrau <esterferrau@gmail.com>
2022-06-09 03:55:36 -0300

    Primera Parte Evalucion

---
+++ CrudMvc/Models/Curriculum.cs
@@ -0,0 +1,21 @@
+﻿namespace CrudMvc.Models
+{
+    public class Curriculum
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+
+        /*[DataType(DataType.Date)]
+
+        public DateTime ReleaseDate { get; set; }*/
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Edad { get; set; }
+    }
+}
+
